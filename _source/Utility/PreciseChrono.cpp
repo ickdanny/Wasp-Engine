b4797ffa40521747a0af97d4184b11533a1d407d
ickdanny <fake_email@gmail.com>
2022-01-17 18:36:03 -0500

    implement non-thread-safe object pool

--- _source/Utility/PreciseChrono.cpp
+++ _source/Utility/PreciseChrono.cpp
@@ -1,33 +1,52 @@
 #include "Utility\PreciseChrono.h"
 
 #include "windowsInclude.h"
+#include <stdexcept>
+
+#include "Container/ObjectPool.h"
 
 namespace wasp::utility {
-	bool sleep100ns(long long hundredNanoseconds) {
-		//implementation from https://gist.github.com/Youka/4153f12cf2e17a77314c
 
-		if (hundredNanoseconds <= 0) {
+	class TimerHandle100ns {
+	private:
+		HANDLE timerHandle{};
+	public:
+		TimerHandle100ns() 
+			: timerHandle{ CreateWaitableTimer(NULL, TRUE, NULL) } {
+			if (!timerHandle) {
+				throw std::runtime_error{ "Error failed to open timer handle" };
+			}
+		}
+
+		bool wait100ns(long long time100ns) {
+			LARGE_INTEGER time{};
+			time.QuadPart = -time100ns;
+
+			if (!SetWaitableTimer(timerHandle, &time, 0, NULL, NULL, FALSE)) {
+				return false;
+			}
+
+			//start and wait for timer
+			WaitForSingleObject(timerHandle, INFINITE);
+
 			return true;
 		}
 
-		HANDLE timerHandle{ CreateWaitableTimer(NULL, TRUE, NULL) };
-		if (!timerHandle) {
-			return false;
-		}
-
-		LARGE_INTEGER time{};
-		time.QuadPart = -hundredNanoseconds;
-
-		if (!SetWaitableTimer(timerHandle, &time, 0, NULL, NULL, FALSE)) {
+		~TimerHandle100ns() {
 			CloseHandle(timerHandle);
-			return false;
+		}
+	};
+
+	bool sleep100ns(long long time100ns) {
+		static auto timerHandlePoolPointer{
+			container::ObjectPool<TimerHandle100ns>::makeObjectPool() 
+		};
+
+		if (time100ns <= 0) {
+			return true;
 		}
 
-		//start and wait for timer
-		WaitForSingleObject(timerHandle, INFINITE);
-		//clean up timer
-		CloseHandle(timerHandle);
-
-		return true;
+		auto timerHandlePointer{ timerHandlePoolPointer->acquire() };
+		return timerHandlePointer->wait100ns(time100ns);
 	}
 }
